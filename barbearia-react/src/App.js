b47537adc33ff0ad661d6dd1fec15b0021e1b2f0
nklbeilke <nicolasbeilke0511@gmail.com>
2026-02-11 17:11:46 -0300

    Add files via upload

--- barbearia-react/src/App.js
+++ barbearia-react/src/App.js
@@ -19,7 +19,7 @@ function App() {
   { id: 4, nome: "Corte + Barba + Sobrancelha", preco: "R$ 70,00" },
   { id: 5, nome: "Cabelo + Sobrancelha", preco: "R$ 40,00" },
   ];
-  const horarios = ["09:00","10:00","11:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00"];
+  const horarios = ["09:00","10:00","11:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00"];
   const diasDisponiveis = [
     new Date(2026, 1, 10),
     new Date(2026, 1, 11),
